5b6e7e50fa89a7b75770ea359947e05d7c01afbb
mazeteam2013 <mazeteam2013@gmail.com>
2022-11-13 18:42:38 +0800

    fix conflict

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # cyclic-express
 Test deploy expressjs to cyclic platform
+
+# Express/EJS Example
+This program demonstrates the following Express/EJS features:
+1. *Default* server routing.
+2. Scripting (conditions and loops) in EJS templates
+
+## Getting Started
+
+### Default server routing
+The server responds to incoming requests, based on pathnames, in following order:
+1. `/read`
+2. `/cafe`
+3. `*` 
+
+The `*` route **redirects** incoming requests having pathnames other than `/read` and `/cafe` to `/read`
+
+### Scripting in EJS templates
+1. Condition is used in `views/details.ejs`
+2. Loops are used in `views/list.ejs
+
+### Installing
+```
+npm install
+```
+### Running
+```
+npm start
+```
+### Testing
+Goto http://localhost:8099
+
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const cafes = [
+	{name: 'Pacific Coffee', address: '30 Good Shepherd Street', seats: 30, id:'001'},
+	{name: 'Starbucks', address: '1 Victory Road', id:'002'},
+	{name: 'UCC Cafe', address: 'New Town Plaza', seats: 120, 'id': '003'}
+];
+
+app.set('view engine', 'ejs');
+
+app.get("/read", (req,res) => {
+	res.render("list", {c: cafes});
+});
+
+app.get('/cafe', (req,res) => {
+	if (req.query.id != null) {
+		for (var i=0; i<cafes.length; i++) {
+			if (cafes[i].id == req.query.id) {
+				res.render('details', {c: cafes[i]});				
+			}
+		}
+		res.status(500).end(req.query.id + ' not found!');
+	} else {
+		res.status(500).end('id missing!');
+	}
+});
+
+app.get('*', (req,res) => {
+	res.redirect('/read');
+});
+
+app.listen(process.env.PORT || 8099);
+// consoe.log
